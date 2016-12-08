cc62b40429d5e2f4c0de125406f7888f1e0676b9
Sam Gilbert <samjgilbert94@gmail.com>
2016-12-08 14:18:06 -0600

    yay no merge conflicts :) refactoring client side

--- public/js/index.js
+++ public/js/index.js
@@ -1,1 +1,11 @@
-console.log("hello world");
+$("#new_room_button").click(function() {
+    $.ajax({
+        url:"/new_room",
+        type: "GET",
+        success: function(res) {
+            window.location.pathname = res;
+        }
+   });
+});
+
+
